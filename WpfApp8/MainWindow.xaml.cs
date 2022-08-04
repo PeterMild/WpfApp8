056264b779a19a46aa83c9ad2fab745627698634
PeterMild <petermildlody@gmail.com>
2022-08-04 19:06:24 +0700

    Initial commit

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+
+        }
+
+    
+               private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+          string addText = (txtBox.Text);
+            arrayList.Add(addText);
+            arrayList.Sort();
+            MessageBox.Show("เพิ่ม " + addText + " เรียบร้อยแล้ว");
+            
+            
+        }
+
+        private void removeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string removeText = (txtBox.Text);
+            arrayList.Remove(removeText);
+            MessageBox.Show("ลบ " + removeText + " เรียบร้อยแล้ว");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+                           
+            foreach(string data in arrayList)
+            {
+                arrayList.Sort();
+                MessageBox.Show(data.ToString());
+                
+            }
+        }
+    }
+}
